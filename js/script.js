008e53ca7a0ce4d0c95e65003dde4aeb1925153c
KingSlme <djgreene100@gmail.com>
2024-01-08 21:27:47 -0500

    Add mobile navigation event

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
+///////////////////////////////////////////////////////////
 // Update copyright year
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+///////////////////////////////////////////////////////////
+// Make mobile navigation work
+const btnNav = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+btnNav.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
